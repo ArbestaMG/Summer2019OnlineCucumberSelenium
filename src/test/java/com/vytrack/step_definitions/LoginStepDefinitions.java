4c33c4bf23bbf520070cc7e57bf1af90c93f5fd8
ofmagden <arbesta41@gmail.com>
2019-12-17 11:46:39 -0500

    First commit

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,26 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class LoginStepDefinitions {
+    @Given("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("user logs in as store manager")
+    public void user_logs_in_as_store_manager() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("user veirifes that {string} page subtitle is displayed")
+    public void user_veirifes_that_page_subtitle_is_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+}
