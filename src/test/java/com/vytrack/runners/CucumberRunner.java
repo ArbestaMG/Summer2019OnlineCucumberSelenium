cd9d89535ec075946dca9c3e12151ab129a2c84d
ofmagden <arbesta41@gmail.com>
2020-01-07 18:16:44 -0500

    First commit

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -5,9 +5,19 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue="com/vytrack/step_definitions"
+@CucumberOptions(//"src/test/resources/features//activities//CalendarEvents.feature // boyle yaparsak da test secmis oluruz
+        features = "src/test/resources/features", // path to features
+        glue="com/vytrack/step_definitions", // path to step definitions
+        dryRun=true, // bu true olmasi durumunda butun kodlarin yazilip yazilmadigini kontrol ediyor
+        // true olunca run edersek, yeni mehtodlar cikarabilir
+        tags="@driver_with_data_table",// buraya @driver yazarsak, sadece driver yazdigimiz calisir
+       // sadece bunun oldugu yer calisir// buraya yazdigimiz calisir
+        // hic birsey yazmazsak, hepsi calisir
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        }// bunu koyduk boylece report olusturdu
+        // target a gidip, show in explorer yaparsan// default reports da bunlar var
 )
 public class CucumberRunner {
 }
